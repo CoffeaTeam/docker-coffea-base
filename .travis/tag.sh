285f568329f7ee37b32ee89370a801d48dad915c
Oksana Shadura <oksana.shadura@cern.ch>
2020-12-04 13:47:47 +0100

    [Travis] Docker tags, inspired from Dask-docker

---
+++ .travis/tag.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+# Login to docker hub
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+$IMAGE="docker-coffea-base"
+
+# If this is not a tagged commit push to the dev label
+if [ "$TRAVIS_TAG" = "" ]; then
+    DEV_IMAGE="$IMAGE:dev"
+    echo "Pushing $DEV_IMAGE"
+    docker tag $IMAGE $DEV_IMAGE
+    docker push $DEV_IMAGE
+
+# If this is a tagged commit push to a new tag label and 'latest'
+else
+    TAG_IMAGE="$IMAGE:$TRAVIS_TAG"
+    echo "Pushing $TAG_IMAGE"
+    docker tag $IMAGE $TAG_IMAGE
+    docker push $TAG_IMAGE
+
+    LATEST_IMAGE="$IMAGE:latest"
+    echo "Pushing $LATEST_IMAGE"
+    docker tag $IMAGE $LATEST_IMAGE
+    docker push $LATEST_IMAGE
+fi
