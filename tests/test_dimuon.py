567d19bd8ec0c17eefa4331764ade02ffe4ea4e0
Oksana Shadura <ksu.shadura@gmail.com>
2023-10-31 15:29:08 +0100

    Add basic test from coffeas.git (coffea/binder/processor.ipynb)

---
+++ tests/test_dimuon.py
@@ -0,0 +1,71 @@
+
+import awkward as ak
+import hist
+from coffea import processor
+from coffea.nanoevents.methods import candidate
+from coffea.nanoevents import NanoEventsFactory, BaseSchema
+
+fileset = {
+    'DoubleMuon': [
+        'root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/Run2012B_DoubleMuParked.root',
+        'root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/Run2012C_DoubleMuParked.root',
+    ],
+    'ZZ to 4mu': [
+        'root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/ZZTo4mu.root'
+    ]
+}
+
+class MyProcessor(processor.ProcessorABC):
+    def __init__(self):
+        pass
+
+    def process(self, events):
+        dataset = events.metadata['dataset']
+        muons = ak.zip(
+            {
+                "pt": events.Muon_pt,
+                "eta": events.Muon_eta,
+                "phi": events.Muon_phi,
+                "mass": events.Muon_mass,
+                "charge": events.Muon_charge,
+            },
+            with_name="PtEtaPhiMCandidate",
+            behavior=candidate.behavior,
+        )
+
+        h_mass = (
+            hist.Hist.new
+            .StrCat(["opposite", "same"], name="sign")
+            .Log(1000, 0.2, 200., name="mass", label="$m_{\mu\mu}$ [GeV]")
+            .Int64()
+        )
+
+        cut = (ak.num(muons) == 2) & (ak.sum(muons.charge, axis=1) == 0)
+        # add first and second muon in every event together
+        dimuon = muons[cut][:, 0] + muons[cut][:, 1]
+        h_mass.fill(sign="opposite", mass=dimuon.mass)
+
+        cut = (ak.num(muons) == 2) & (ak.sum(muons.charge, axis=1) != 0)
+        dimuon = muons[cut][:, 0] + muons[cut][:, 1]
+        h_mass.fill(sign="same", mass=dimuon.mass)
+
+        return {
+            dataset: {
+                "entries": len(events),
+                "mass": h_mass,
+            }
+        }
+
+    def postprocess(self, accumulator):
+        pass
+
+def test_processor_dimu_mass():
+    iterative_run = processor.Runner(executor = processor.IterativeExecutor(compression=None),
+                                     schema=BaseSchema,
+                                     maxchunks=10
+                                     )
+    out = iterative_run(fileset,
+                        treename="Events",
+                        processor_instance=MyProcessor(),
+                        )
+    assert out["DoubleMuon"] == 1000560
